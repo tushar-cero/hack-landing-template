b0d8d3dbe116a06b0d51812f8ce945ededaa5178
Tushar <tusharsingh.official@icloud.com>
2024-06-27 00:33:32 +0530

    landing, desktop, not responsive

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <main>
+        <header>
+          <div className="logo"></div>
+          <ul className="flex gap-8 justify-center items-center px-6">
+            <a href="https://www.google.com">Solutions</a>  
+            <a href="https://www.google.com">Developers</a>  
+            <a href="https://www.google.com">Ecosystem</a>  
+            <a href="https://www.google.com">Blogs</a>  
+            <a href="https://www.google.com">About</a>  
+          </ul>
+        </header>
+        <section>
+          <div className="hero-container">
+            <h1>Stream money every second.</h1>
+            <p>placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla</p>
+            <div className="btn-container">
+              <button className="btn-primary">Launch App</button>
+              <button className="btn-secondary">Book a demo</button>
+            </div>
+            <div className="label">
+              <p>Total Volume</p>
+              <h2>$395,764,369</h2>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <article className="cards-section">
+        <h1>Why us /Product Name/?</h1>
+        <div className="card-container">
+          <div className="card">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#fff" fill="none"><path d="M13.7276 3.44418L15.4874 6.99288C15.7274 7.48687 16.3673 7.9607 16.9073 8.05143L20.0969 8.58575C22.1367 8.92853 22.6167 10.4206 21.1468 11.8925L18.6671 14.3927C18.2471 14.8161 18.0172 15.6327 18.1471 16.2175L18.8571 19.3125C19.417 21.7623 18.1271 22.71 15.9774 21.4296L12.9877 19.6452C12.4478 19.3226 11.5579 19.3226 11.0079 19.6452L8.01827 21.4296C5.8785 22.71 4.57865 21.7522 5.13859 19.3125L5.84851 16.2175C5.97849 15.6327 5.74852 14.8161 5.32856 14.3927L2.84884 11.8925C1.389 10.4206 1.85895 8.92853 3.89872 8.58575L7.08837 8.05143C7.61831 7.9607 8.25824 7.48687 8.49821 6.99288L10.258 3.44418C11.2179 1.51861 12.7777 1.51861 13.7276 3.44418Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg>
+            </span>
+            <h4>Products</h4>
+            <p>Standard chunk of Lorem Ipsum used since the 1500s is showed below
+            for those interested.</p>
+          </div>
+          <div className="card">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#fff" fill="none"><path d="M13.7276 3.44418L15.4874 6.99288C15.7274 7.48687 16.3673 7.9607 16.9073 8.05143L20.0969 8.58575C22.1367 8.92853 22.6167 10.4206 21.1468 11.8925L18.6671 14.3927C18.2471 14.8161 18.0172 15.6327 18.1471 16.2175L18.8571 19.3125C19.417 21.7623 18.1271 22.71 15.9774 21.4296L12.9877 19.6452C12.4478 19.3226 11.5579 19.3226 11.0079 19.6452L8.01827 21.4296C5.8785 22.71 4.57865 21.7522 5.13859 19.3125L5.84851 16.2175C5.97849 15.6327 5.74852 14.8161 5.32856 14.3927L2.84884 11.8925C1.389 10.4206 1.85895 8.92853 3.89872 8.58575L7.08837 8.05143C7.61831 7.9607 8.25824 7.48687 8.49821 6.99288L10.258 3.44418C11.2179 1.51861 12.7777 1.51861 13.7276 3.44418Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg>
+            </span>
+            <h4>Products</h4>
+            <p>Standard chunk of Lorem Ipsum used since the 1500s is showed below
+            for those interested.</p>
+          </div>
+        </div>
+      </article>
+      
+      <article className="features">
+        <div className="feature-wrapper">
+          <div className="feature-info">
+            <h2>1. Feature</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+            </ul>
+          </div>
+          <div className="feature-img"></div>
+        </div>
+        <div className="feature-wrapper">
+          <div className="feature-info">
+            <h2>2. Feature</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+            </ul>
+          </div>
+          <div className="feature-img"></div>
+        </div>
+      </article>
+
+      <footer>
+        <div className="logo"></div>
+        <ul className="flex gap-8 justify-center items-center px-6">
+          <a href="https://www.google.com">Solutions</a>  
+          <a href="https://www.google.com">Developers</a>  
+          <a href="https://www.google.com">Ecosystem</a>  
+          <a href="https://www.google.com">Blogs</a>  
+          <a href="https://www.google.com">About</a>  
+        </ul>
+      </footer>
+    </React.Fragment>
+  );
+}
+
+export default App;
